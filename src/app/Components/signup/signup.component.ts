5721d55b7992ba634eb58152cbabfd33567de12a
Hragab97 <ragab1997@gmail.com>
2024-08-24 13:37:34 +0300

    third commit

--- src/app/Components/signup/signup.component.ts
+++ src/app/Components/signup/signup.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AlertComponent } from "../../shared/ui/alert/alert.component";
+import { confirmPassword } from '../../shared/utils/confirm-password.utils';
+import { signUpValidators } from '../../shared/validators/register.validators';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, AlertComponent],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
 
+  registerform: FormGroup = new FormGroup({
+    name: new FormControl(null, signUpValidators.name),
+    email: new FormControl(null, signUpValidators.email),
+    password: new FormControl(null, signUpValidators.password),
+    rePassword: new FormControl(null)
+  },confirmPassword)
+
+  sendData = () => {
+
+    if (this.registerform.valid) {
+      console.log(this.registerform.value)
+
+    }
+  }
 }
